a61ddd1dd8ea49bd00b5e570e10b22fdf2a0ef4d
Andrew <gavriloff.andrew@gmail.com>
2024-04-05 12:12:39 +0300

    refactored generate_diff()

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -9,22 +9,21 @@ def open_json(path):
 
 def generate_diff(file1, file2):
     file1, file2 = open_json(file1), open_json(file2)
-    common_keys = []
     result = []
+
     for key in file1:
         if key in file2:
+
             if file1[key] == file2[key]:
                 result.append(f'  {key}: {file1[key]}')
-                common_keys.append(key)
             else:
                 result.append(f'- {key}: {file1[key]}')
                 result.append(f'+ {key}: {file2[key]}')
-                common_keys.append(key)
+            file2.pop(key)
+
         else:
             result.append(f'- {key}: {file1[key]}')
 
-    for key in common_keys:
-        file2.pop(key)
     for key in file2:
         result.append(f'+ {key}: {file2[key]}')
 
